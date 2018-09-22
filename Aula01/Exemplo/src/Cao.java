a3a727f63a0c0cde077e79de781425ef04c9a7ea
edersonlrf <edersonluis.rf@gmail.com>
2018-09-22 15:47:27 -0300

    aula 01 exemplo

---
+++ Aula01/Exemplo/src/Cao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ederson.fernandes
+ */
+public class Cao {
+    
+    public String nome;
+    public int peso;
+    public String corOlhos;
+    public int quantPatas;
+    public String situacao;
+    
+    public void latir() {
+        // Método LATIR.
+    }
+    
+    public void andar() {
+        // Método ANDAR.
+    }
+    
+    public void comer() {
+        // Método COMER.
+    }
+    
+    public void dormir() {
+        // Método DORMIR.
+    }
+    
+}
